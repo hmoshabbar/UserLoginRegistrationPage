846ec4ec571ea9352bf5eb54691773303caab560
hmoshabbar <talktomoshabbar@gmail.com>
2017-06-21 18:27:18 +0530

    User login Registration Page using Django

---
+++ login/admin.py
@@ -0,0 +1,20 @@
+#from django.contrib import admin
+
+# Register your models here.
+#from django import forms
+
+
+# Register your models here.
+#from .models import UsersList
+
+#class UserListModelAdmin(admin.ModelAdmin):
+    #list_display=('first_name','last_name','username','email','phone','address') # list will display like this
+  
+    #search_fields=['name']  #This is for searching purpose
+     # For filtering purpose like Today,anyday,like this way.
+     
+    
+    #class Meta:
+        #model=UsersList
+        
+#admin.site.register(UsersList,UserListModelAdmin)
---
+++ login/models.py
@@ -0,0 +1,18 @@
+#from __future__ import unicode_literals
+
+#from django.db import models
+
+# Create your models here.
+
+
+#class UsersList(models.Model):
+    #first_name = models.CharField(max_length=50, primary_key=True)
+    #last_name=models.CharField(max_length=230)
+    #email=models.EmailField(max_length=200)
+    #address=models.CharField(max_length=15)
+    #phone=models.CharField(max_length=200)
+    #username=models.CharField(max_length=200)
+    #password=models.CharField(max_length=300)
+
+    #def __str__(self):
+        #return self.name
